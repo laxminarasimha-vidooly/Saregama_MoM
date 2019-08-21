0d5782a0ed49200c12807c059c15a779137e0f58
laxminarasimha-vidooly <54302572+laxminarasimha-vidooly@users.noreply.github.com>
2019-08-21 12:51:29 +0530

    Update test_model.py

--- Old_links/test_model.py
+++ Old_links/test_model.py
@@ -1,13 +1,5 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Fri Jun  7 15:04:37 2019
-
-@author: chiru
-"""
-
+#Importing libraries
 import os
-os.chdir("C:\\Users\\chiru\\Desktop\\Vidooly\\ad_vs_organic_monthly")
-
 from fetch_video_stats import get_video_ids
 #from channel_video_stats import get_ch_video_ids
 import pandas as pd
@@ -19,29 +11,13 @@ model_1=joblib.load('model_1.pkl')
 model_2=joblib.load('model_2.pkl')
 model_3=joblib.load('model_3.pkl')
 
-
-
-
 def derive_columns_1(df):
     
     df['views_c']=df['views']/1000
     df['eng_c']=df['engagement']/50
     df['log_of_v/e']=np.log(df['views']/df['engagement'])
     df['eng_per']=(df['engagement']/df['views'])*100
-#    df['views_c_sq']=df['views_c']**2
-##    df['eng_c_sq']=df['eng_c']**2
-#    df['published_date']=df['published_date'].map(lambda x:x.split('T')[0])
-#
-#    df['published_date']=pd.to_datetime(df['published_date'])
-#    
-#    d0=date(2019,6,6)
-#    d0=pd.to_datetime(d0)
-#    d1=df['published_date']
-#    delta=d0-d1
-#    df['days']=delta.astype('str').map(lambda x:x.split(' ')[0]).astype('int')
-#    df['views/days']=df['views']/df['days']
-#    df['eng/days']=df['engagement']/df['days']
-#    df['ratio']=df['views/days']/df['eng/days']    
+
     return df
 
 def derive_columns_2(df):
@@ -50,18 +26,9 @@ def derive_columns_2(df):
     df['eng_c']=df['engagement']/500
     df['log_of_v/e']=np.log(df['views']/df['engagement'])
     df['eng_per']=(df['engagement']/df['views'])*100
-#    df['published_date']=df['published_date'].map(lambda x:x.split('T')[0])
-#    df['published_date']=pd.to_datetime(df['published_date'])
-#    d0=date(2019,6,6)
-#    d0=pd.to_datetime(d0)
-#    d1=df['published_date']
-#    delta=d0-d1
-#    df['days']=delta.astype('str').map(lambda x:x.split(' ')[0]).astype('int')
-#    df['views/days']=df['views']/df['days']
-#    df['eng/days']=df['engagement']/df['days']
-#    df['ratio']=df['views/days']/df['eng/days']    
-    return df
 
+    return df
+#Ad prediction (videos)
 def ad_vs_organic(df):
     
     v_id=get_video_ids()
@@ -120,7 +87,7 @@ def ad_vs_organic(df):
 
 #ad_vs_organic('test.csv')
     
-
+#Ad prediction (channels)
 def ad_vs_organic_channel(filename,start_date,end_date,gap_days=0):
     print(" inside channel_id_stats")
     print(" start_date:{0} ,  end_date:{1}  and gap_days:{2} entered by user".format(start_date,end_date,gap_days))
@@ -178,10 +145,3 @@ def ad_vs_organic_channel(filename,start_date,end_date,gap_days=0):
     result_df['Organic_views']=(result_df['views']-result_df['AD_views'])
 #    result_df=result_df[['video_id_list','channel_id','views','likes','dislikes','comment','publilshed_date','AD_views','Organic_views']]
     return result_df
-
-    
-    
-#ad_vs_organic_channel('ch_id.csv',date(2019,1,1),date(2019,5,20))
-
-
-
